9b444ed729ef67074fe3e38f1b71d27c682d228e
Jeremy Pruitt <jeremypruitt@me.com>
2022-08-15 03:18:10 -0700

    Refactor much of main file

--- main.py
+++ main.py
@@ -3,23 +3,83 @@ import json
 import requests
 
 DEBUG = os.environ.get('DEBUG')
-if DEBUG:
-    print("\n\n# ---------------------------------------------------------------\n")
-    print("# All Env Vars\n")
-    print("# ---------------------------------------------------------------\n")
-    print(f"{os.environ}\n")
-    print("# ---------------------------------------------------------------\n\n")
+
 GITHUB_BASE_URL = 'https://api.github.com'
-LEETCODE_BASE_URL = "https://www.leetcode.com"
-EMOJI = { "easy": "🟢", "medium": "🟡", "hard": "🔴" }
 GITHUB_REPOSITORY = os.environ.get('INPUT_GITHUB_REPOSITORY')
 GITHUB_TOKEN = os.environ.get("INPUT_GITHUB_TOKEN")
 
-# ---------------------------------------------------------------------
-# Create Github Issue
-# ---------------------------------------------------------------------
+LEETCODE_BASE_URL = "https://leetcode.com"
+EMOJI = { "easy": "🟢", "medium": "🟡", "hard": "🔴" }
+
+def get_leetcode_daily_problem():
+    body = """
+      query questionOfToday {
+        activeDailyCodingChallengeQuestion {
+          date
+          link
+          question {
+            acRate
+            codeDefinition
+            content
+            difficulty
+            dislikes
+            enableRunCode
+            frontendQuestionId: questionFrontendId
+            likes
+            metaData
+            questionId
+            sampleTestCase
+            similarQuestions
+            status
+            stats
+            title
+            titleSlug
+            topicTags {
+              name
+              id
+              slug
+            }
+          }
+        }
+      }
+    """
+
+    payload = { "query": body, "operationName":"questionOfToday" }
+
+    response = requests.post(url=f"{LEETCODE_BASE_URL}/graphql", json=payload)
+
+    if response.status_code != 200:
+        print(f"❌  Leetcode Query Response Status Code: {response.status_code}")
+        print(f"    Leetcode Query URL: {LEETCODE_BASE_URL}/graphql")
+        return False
+
+    print(f"✅  Leetcode Query Response Status Code: {response.status_code}")
+    json_response = response.json()
+    return response.json()
+
+def generate_github_issue_body(question) -> str:
+    problem_url = f"{LEETCODE_BASE_URL}{question['link']}"
+
+    body = f"# {question['question']['questionId']}. {question['question']['title']}\n"
+
+    body += "\n"
+    body += f"[🔗 Problem]({problem_url}) [🧵 Discussion]({problem_url}discuss/) [🙋 Solution]({problem_url}solution/)\n"
+
+    difficulty = question['question']['difficulty'].lower()
+    body += "\n"
+    body += f"| Difficulty | {EMOJI[difficulty]} |\n"
+    body += "| :-- | :-: |\n"
+    body += f"| Accept Rate | {question['question']['acRate']:.1f}% |\n"
+
+    body += "\n"
+    body += f"🏷️  {process_tags(question['question']['topicTags'])}\n"
+
+    body += f"{question['question']['content']}\n"
+
+    return body
+
 def create_github_issue(title,body):
-    if DEBUG: print(f'GITHUB_REPOSITORY: {GITHUB_REPOSITORY}')
+    if DEBUG: print(f"👉  GITHUB_REPOSITORY: {GITHUB_REPOSITORY}")
 
     repos_url  = f'{GITHUB_BASE_URL}/repos'
     repo_url   = f'{repos_url}/{GITHUB_REPOSITORY}'
@@ -32,123 +92,38 @@ def create_github_issue(title,body):
     payload = json.dumps({'title': title, 'body': body})
 
     if DEBUG:
-        print(f'POSTing to {issues_url} with JSON payload:')
+        print(f"👉  POSTing to {issues_url} with JSON payload:")
         print(payload)
 
     response = session.post(issues_url, data=payload, headers=headers)
     if response.status_code != 201:
-        print(f'Could not create new Github:')
-        print(f'    Status Code: {response.status_code}')
-        print(f'    Response: {response.content}')
+        print(f"❌  Could not create new Github issue!")
+        print(f"    Status Code: {response.status_code}")
+        print(f"    Response: {response.content}")
+        return False
 
     issue_number = response.json().get("number")
     issue_title  = response.json().get("title")
-    print('Successfully created Issue #{issue_number}: {issue_title}')
-    if DEBUG: print(response.json())
+    print(f"✅  Successfully created Issue #{issue_number}: {issue_title}")
+    if DEBUG: print(f"👉  {response.json()}")
+
+    return True
 
 def process_tags(tags) -> str:
     tag_url = f'{LEETCODE_BASE_URL}/tag/'
     return ' '.join(f'#[{t["slug"]}]({tag_url}{t["slug"]}/)' for t in tags)
  
 def main():
-    url = "https://leetcode.com/graphql"
-    body = """
-      query questionOfToday {
-        activeDailyCodingChallengeQuestion {
-          date
-          userStatus
-          link
-          question {
-            acRate
-            difficulty
-            freqBar
-            frontendQuestionId: questionFrontendId
-            hasSolution
-            hasVideoSolution
-            isFavor
-            paidOnly: isPaidOnly
-            status
-            title
-            titleSlug
-            codeDefinition
-            codeSnippets {
-                lang
-                langSlug
-                code
-                __typename
-            }
-            content
-            dislikes
-            enableRunCode
-            exampleTestcases
-            hints
-            likes
-            metaData
-            questionId
-            sampleTestCase
-            similarQuestions
-            stats
-            topicTags {
-              name
-              id
-              slug
-              translatedName
-              __typename
-            }
-            translatedContent
-            translatedTitle
-          }
-        }
-      }
-    """
+    leetcode_json_response = get_leetcode_daily_problem()
+    question = leetcode_json_response["data"]["activeDailyCodingChallengeQuestion"]
 
-    payload = {
-      "query": body,
-      "operationName":"questionOfToday" }
+    github_issue_body = generate_github_issue_body(question)
+    github_issue_title = f"LC Daily: {question['question']['questionId']}. {question['question']['title']}"
 
-    response = requests.post(url=url, json=payload)
-    print(f'response status code: {response.status_code}')
-    if response.status_code == 200:
-        json_response = response.json()
-        pretty_json_response = json.dumps(json_response, indent=3)
+    print(f"ℹ️   github_issue_title: {github_issue_title}")
+    if DEBUG: print(f"👉  github_issue_body:\n{github_issue_body}")
 
-        data = json_response["data"]
-        question = data["activeDailyCodingChallengeQuestion"]
-
-        problem_url = f"{LEETCODE_BASE_URL}{question['link']}"
-
-        new_response = f"# {question['question']['questionId']}. {question['question']['title']}\n"
-
-        new_response += "\n"
-        new_response += f"[🔗 Problem]({problem_url}) [🧵 Discussion]({problem_url}discuss/) [🙋 Solution]({problem_url}solution/)\n"
-
-        difficulty = question['question']['difficulty'].lower()
-        new_response += "\n"
-        new_response += f"| Difficulty | {EMOJI[difficulty]} |\n"
-        new_response += "| :-- | :-: |\n"
-        new_response += f"| Accept Rate | {question['question']['acRate']:.1f}% |\n"
-
-        new_response += "\n"
-        new_response += f"🏷️  {process_tags(question['question']['topicTags'])}\n"
-
-        new_response += f"{question['question']['content']}\n"
-
-        if DEBUG:
-            new_response += "\n"
-            new_response += "## Raw JSON Payload\n"
-            new_response += "<details>\n"
-            new_response += "    <summary>Click to expand...</summary>\n\n"
-            new_response += "```json\n"
-            new_response += f"{pretty_json_response}\n"
-            new_response += "```\n\n"
-            new_response += "</details>\n"
-
-        print(f'response: {new_response}')
-        create_github_issue(f"TEST - LC Daily: {question['question']['questionId']}. {question['question']['title']}",new_response)
-
-
-
-    # ---------------------------------------------------------------------
+    return create_github_issue(github_issue_title,github_issue_body)
 
 if __name__ == "__main__":
     main()
